914eeb9ff4fd45fa41dcdabef35a4407954534f4
Maria Vass <vassmarcsi88@gmail.com>
2018-10-25 17:25:51 +0200

    add, remove, update

--- SzemelyAdatbazis/src/Main.java
+++ SzemelyAdatbazis/src/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SzemelyDB adatbazis = new SzemelyDB();
+
+		adatbazis.addSzemely("Kiss", "Imre", "1972-09-12");
+
+		adatbazis.removeSzemely("3");
+		
+		adatbazis.updateSzemely("2", "Minta", "Katalin", "1972-09-12");
+		
+		adatbazis.showAllSzemely();
 
 	}
 
